2bbe3c9d9b8d3f7275c94c937d8e05a3bc869c8c
firatoncu <f.firatoncu@gmail.com>
2025-03-09 19:48:04 +0300

    adding histogram indicator

--- src/check_condition.py
+++ src/check_condition.py
@@ -1,5 +1,5 @@
 import ta # type: ignore
-from src.indicators import no_crossing_last_10, last500_macd_check
+from src.indicators import no_crossing_last_10, last500_macd_check, last500_histogram_check
 
 
 # Alış koşulları
@@ -8,6 +8,7 @@ def check_buy_conditions(df, logger):
         macd = ta.trend.MACD(df['close'], window_slow=26, window_fast=12, window_sign=9)
         macd_line = macd.macd()
         signal_line = macd.macd_signal()
+        hist_line = macd.macd_diff()
 
         slope_macd = macd_line.iloc[-1] - macd_line.iloc[-2]
         slope_signal = signal_line.iloc[-1] - signal_line.iloc[-2]
@@ -20,8 +21,9 @@ def check_buy_conditions(df, logger):
         buyCondC = (macd_line.iloc[-1] > signal_line.iloc[-1]) and (macd_line.iloc[-2] <= signal_line.iloc[-2])
         buyCondD = no_crossing_last_10(macd_line, signal_line, logger)
         buyCondE = last500_macd_check(macd_line, 500, logger)
+        buyCondF = last500_histogram_check(hist_line, 300, logger)
 
-        return buyCondA and buyCondB and buyCondC and buyCondD and buyCondE
+        return buyCondA and buyCondB and buyCondC and buyCondD and buyCondE and buyCondF
     except Exception as e:
         logger.error(f"check_buy_conditions hatası: {e}")
         return False
@@ -32,6 +34,7 @@ def check_sell_conditions(df, logger):
         macd = ta.trend.MACD(df['close'], window_slow=26, window_fast=12, window_sign=9)
         macd_line = macd.macd()
         signal_line = macd.macd_signal()
+        hist_line = macd.macd_diff()
 
         slope_macd = macd_line.iloc[-1] - macd_line.iloc[-2]
         slope_signal = signal_line.iloc[-1] - signal_line.iloc[-2]
@@ -43,10 +46,11 @@ def check_sell_conditions(df, logger):
         sellCondB = -(slope_macd ** 2) < -(slope_signal ** 2) - threshold
         sellCondC = (macd_line.iloc[-1] < signal_line.iloc[-1]) and (macd_line.iloc[-2] >= signal_line.iloc[-2])
         sellCondD = no_crossing_last_10(macd_line, signal_line, logger)
-        sellCondE = slope_macd < 0
-        sellCondF = last500_macd_check(macd_line, 500, logger)
+        sellCondE = last500_macd_check(macd_line, 500, logger)
+        sellCondF = last500_histogram_check(hist_line, 300, logger)
 
         return sellCondA and sellCondB and sellCondC and sellCondD and sellCondE and sellCondF
+    
     except Exception as e:
         logger.error(f"check_sell_conditions hatası: {e}")
         return False
--- src/indicators.py
+++ src/indicators.py
@@ -14,7 +14,6 @@ def no_crossing_last_10(macd_line, signal_line, logger):
 # Son 500 mumda MACD kontrolü
 def last500_macd_check(macd_line, lookback_period, logger):
     try:
-        current_macd = macd_line.iloc[-1]
         macd_last_500 = macd_line.tail(lookback_period)
         macd_variance = macd_last_500.max() + abs(macd_last_500.min())
         macd_threshold = macd_variance * 0.2
@@ -23,4 +22,17 @@ def last500_macd_check(macd_line, lookback_period, logger):
         return False
     except Exception as e:
         logger.error(f"last500_macd_check hatası: {e}")
+        return False
+    
+def last500_histogram_check(histogram, lookback_period, logger, side_):
+    try:
+        last10_avg = (histogram.tail(10).abs()).avg()
+        histogram_last_lookback = histogram.tail(lookback_period)
+        histogram_variance = histogram_last_lookback.max() + abs(histogram_last_lookback.min())
+        histogram_threshold = histogram_variance * 0.3
+        if abs(last10_avg) >= histogram_threshold:
+            return True
+        return False
+    except Exception as e:
+        logger.error(f"last500_histogram_check hatası: {e}")
         return False
