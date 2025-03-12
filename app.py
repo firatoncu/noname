b0ac5afff90973ed8711a53aec63da84c981e8b7
firatoncu <f.firatoncu@gmail.com>
2025-03-12 20:09:16 +0300

    Refactor leverage adjustment logic and replace adjust_leverage function with initial_adjustments

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from binance.client import Client
 from utils.load_config import load_config
-from utils.adjust_leverage import adjust_leverage
+from utils.initial_adjustments import initial_adjustments
 from utils.logging import logger_func
 from src.open_position import open_position
-from globals import set_clean_sell_signal, set_clean_buy_signal
 
 logger = logger_func()
 config = load_config()
@@ -15,11 +14,8 @@ max_open_positions = symbol_config['max_open_positions']
 symbols = symbol_config['symbols']
 
 client = Client(api_config['api_key'], api_config['secret'])
-adjust_leverage(leverage, symbols, client, logger)
+initial_adjustments(leverage, symbols, client, logger)
 
-# Set global variables to False at the start of the code execution
-set_clean_sell_signal(False)
-set_clean_buy_signal(False)
 
 while True:
     open_position(max_open_positions, symbols, logger, 
--- utils/initial_adjustments.py
+++ utils/initial_adjustments.py
@@ -1,7 +1,11 @@
-def adjust_leverage(leverage, symbols, client,logger):
+from globals import set_clean_sell_signal, set_clean_buy_signal
+
+def initial_adjustments(leverage, symbols, client,logger):
     # Her sembol için kaldıracı ayarla
     for symbol in symbols:
         try:
+            set_clean_sell_signal(False, symbol)
+            set_clean_buy_signal(False, symbol)
             client.futures_change_leverage(symbol=symbol, leverage=leverage)
             logger.info(f"{symbol} için kaldıraç {leverage}x olarak ayarlandı")
         except Exception as e:
