32a4729b244edb5c3c66fb27eda1034a031efab9
firatoncu <f.firatoncu@gmail.com>
2025-03-16 19:26:44 +0300

    Refactor current status output formatting for buy conditions

--- utils/current_status.py
+++ utils/current_status.py
@@ -24,14 +24,14 @@ async def current_status(symbols):
         # Alım durumu satırı
         buy_status = (
             f"{Style.BRIGHT}{symbol}{Style.RESET_ALL}"
-            f"\nBuyCondition1: {Fore.GREEN if buyCondA else Fore.RED}{buyCondA}{Style.RESET_ALL},       "
-            f"BuyCondition2: {Fore.GREEN if buyCondB else Fore.RED}{buyCondB}{Style.RESET_ALL},     "
-            f"BuyCondition3:{Fore.GREEN if buyCondC else Fore.RED}{buyCondC}{Style.RESET_ALL}       "
+            f"\nBuyCondition1 : {Fore.GREEN if buyCondA else Fore.RED}{buyCondA}{Style.RESET_ALL},     "
+            f"BuyCondition2 : {Fore.GREEN if buyCondB else Fore.RED}{buyCondB}{Style.RESET_ALL},    "
+            f"BuyCondition3 : {Fore.GREEN if buyCondC else Fore.RED}{buyCondC}{Style.RESET_ALL}       "
         )
 
         # Satım durumu satırı
         sell_status = (
-            f"\nSellCondition1: {Fore.GREEN if sellCondA else Fore.RED}{sellCondA}{Style.RESET_ALL},      "
+            f"\nSellCondition1: {Fore.GREEN if sellCondA else Fore.RED}{sellCondA}{Style.RESET_ALL},     "
             f"SellCondition2: {Fore.GREEN if sellCondB else Fore.RED}{sellCondB}{Style.RESET_ALL},    "
             f"SellCondition3: {Fore.GREEN if sellCondC else Fore.RED}{sellCondC}{Style.RESET_ALL}   "
             f"\n"
